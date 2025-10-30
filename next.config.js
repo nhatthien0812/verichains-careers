678a6ae2b0a0ca791080541a1b639f54f96d772f
thienlpn <thienlpn0812@gmail.com>
2025-10-30 10:29:57 +0700

    chore: for build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "dist",
   eslint: {
     // Ignore ESLint errors during production builds (CI/Vercel)
     ignoreDuringBuilds: true,
